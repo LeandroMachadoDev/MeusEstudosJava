810750e1860daa611d8068f4fc86214ce11bdb70
Leandro Machado <107806624+Leandro50@users.noreply.github.com>
2026-02-15 12:37:54 -0400

    Primeiro exercicio If- Else: Par ou Impar

---
+++ ExercicioIf-Else01/src/ExercicioParImpar.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ExercicioParImpar {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite um numero: ");
+		int numero = sc.nextInt();
+		if (numero % 2 == 0) {
+			System.out.printf("O numero %d é par %n", numero);
+		}
+		else {
+			System.out.printf("O numero %d  é impar %n", numero);
+		}
+
+	}
+
+}
